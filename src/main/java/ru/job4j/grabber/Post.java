11f345580d7e8c396afe93802efbc4f906937889
dmn800 <nefediev.avid@yandex.ru>
2024-01-05 03:19:01 +0500

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,42 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Post {
+
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(title, post.title) && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Post.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("title='" + title + "'")
+                .add("link='" + link + "'")
+                .add("description='" + description + "'")
+                .add("created=" + created)
+                .toString();
+    }
+}
